570a214c4adb132d503a6499c20b3e26458aec69
Alison Ryan <Ryan.alis@northeastern.edu>
2024-09-08 20:33:02 -0400

    minor fixes

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,14 +1,14 @@
 import dotenv from 'dotenv';
+import { resolve } from 'path';
 
-dotenv.config();
+dotenv.config({ path: resolve(__dirname, '../../../.env') });
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = process.env.MONGO_URL || '';
 
 const SERVER_PORT = process.env.SERVER_PORT
-  ? Number(process.env.SERVER_PORT)
-  : 1337;
+  ? Number(process.env.SERVER_PORT) : 3000;
 
 const SUPABASE_URL = process.env.SUPABASE_URL || '';
 const SUPABASE_KEY = process.env.SUPABASE_KEY || '';
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,6 +11,10 @@ import mongoose from 'mongoose';
 import { config } from './config/config';
 import routes from './routes';
 import { sessionMiddleware } from './config/sessionConfig';
+import { resolve } from 'path';
+import dotenv from 'dotenv'
+
+dotenv.config({ path: resolve(__dirname, '../.env') });
 
 const router = express();
 
