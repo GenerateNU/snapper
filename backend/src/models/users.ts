f00d9747325631c5a4e15c8838aac8bc77724078
Alison Ryan <Ryan.alis@northeastern.edu>
2024-09-16 20:13:38 -0400

    schema

--- backend/src/models/users.ts
+++ backend/src/models/users.ts
@@ -4,6 +4,12 @@ const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   supabaseId: { type: String, required: true, unique: true },
+  badges: { type: String, enum: ['PLACEHOLDER'] },
+  diveLogs: [{ type: mongoose.Schema.Types.ObjectId, ref: 'DiveLog' }],
+  fishCollected: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Fish' }],
+  followers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  following: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  profilePicture: { type: String }, // aws s3 url
 });
 
 export const UserModel = mongoose.model('User', UserSchema);
