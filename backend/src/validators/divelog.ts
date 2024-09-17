422a70d8c0527c7bea8852046f083dffe609c006
mai <thisismainguyen@gmail.com>
2024-09-17 11:40:27 -0400

    feat: update validators to include optional field

--- backend/src/validators/divelog.ts
+++ backend/src/validators/divelog.ts
@@ -1,11 +1,11 @@
 const { body, param } = require('express-validator');
 
 export const createDiveLogValidation = [
-    body('user').notEmpty().withMessage('User is required').isMongoId(),
+    body('user').notEmpty().withMessage('User is required').isMongoId().withMessage('Invalid user ID'),
     body('date').isISO8601().withMessage('Invalid date format'),
+    body('location.type').equals('Point').withMessage('Location type must be "Point"'),
     body('location.coordinates')
-        .isArray({ min: 2, max: 2 })
-        .withMessage('Coordinates must be an array of two numbers')
+        .isArray({ min: 2, max: 2 }).withMessage('Coordinates must be an array of two numbers')
         .custom((coords: any[]) => {
             if (coords.length !== 2) {
                 throw new Error('Coordinates must be an array of two numbers');
@@ -14,16 +14,28 @@ export const createDiveLogValidation = [
                 throw new Error('Each coordinate must be a number');
             }
             return true;
-        })
+        }),
+    body('time').optional().isString().withMessage('Time must be a string'),
+    body('duration').optional().isNumeric().withMessage('Duration must be a number'),
+    body('depth').optional().isNumeric().withMessage('Depth must be a number'),
+    body('photos').optional().isArray().withMessage('Photos must be an array of strings')
+        .custom((photos: any[]) => {
+            if (!photos.every(val => typeof val === 'string')) {
+                throw new Error('Each photo URL must be a string');
+            }
+            return true;
+        }),
+    body('description').optional().isString().withMessage('Description must be a string'),
 ];
+
 
 export const updateDiveLogValidation = [
     body('user').optional().isMongoId().withMessage('Invalid user ID'),
     body('date').optional().isISO8601().withMessage('Invalid date format'),
+    body('location.type').optional().equals('Point').withMessage('Location type must be "Point"'),
     body('location.coordinates')
         .optional()
-        .isArray({ min: 2, max: 2 })
-        .withMessage('Coordinates must be an array of two numbers')
+        .isArray({ min: 2, max: 2 }).withMessage('Coordinates must be an array of two numbers')
         .custom((coords: any[]) => {
             if (coords.length !== 2) {
                 throw new Error('Coordinates must be an array of two numbers');
@@ -32,8 +44,20 @@ export const updateDiveLogValidation = [
                 throw new Error('Each coordinate must be a number');
             }
             return true;
-        })
+        }),
+    body('time').optional().isString().withMessage('Time must be a string'),
+    body('duration').optional().isNumeric().withMessage('Duration must be a number'),
+    body('depth').optional().isNumeric().withMessage('Depth must be a number'),
+    body('photos').optional().isArray().withMessage('Photos must be an array of strings')
+        .custom((photos: any[]) => {
+            if (!photos.every(val => typeof val === 'string')) {
+                throw new Error('Each photo URL must be a string');
+            }
+            return true;
+        }),
+    body('description').optional().isString().withMessage('Description must be a string'),
 ];
+
 
 export const idFormatValidation = [
     param('id').notEmpty().withMessage('ID is required').isMongoId().withMessage('Invalid ID format')
