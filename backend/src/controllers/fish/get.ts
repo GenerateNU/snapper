455f92cc5b35f84fe8aa6953eb84f176ea4b5945
Stone Liu <liu.sto@northeastern.edu>
2024-09-20 16:32:33 -0400

    Get by Id dynamic endpoint

---
+++ backend/src/controllers/fish/get.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import { Fish } from '../../models/fish';
+
+export const getById = async (req: express.Request, res: express.Response) => {
+  const id = req.params.id;
+  const fish = await Fish.findById(id)
+  if (!fish) return res.status(404).json({ message: "Fish not found" });
+  return res.status(200).json(fish);
+}
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import authentification from './authentification';
 import healthcheck from './healthcheck';
+import fish from './fish';
 
 const router = express.Router();
 export default (): express.Router => {
   authentification(router);
   healthcheck(router);
+  fish(router);
   return router;
 };
