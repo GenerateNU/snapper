c412d34f27dd7266658aaa0cd268c681c26e9bbb
Stone Liu <132910172+stoneliuCS@users.noreply.github.com>
2024-10-12 17:50:58 -0400

    Image picker component (#13)

---
+++ frontend/components/image-picker.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { Image, TouchableOpacity, View, Text } from 'react-native';
+import * as ExpoImagePicker from 'expo-image-picker';
+
+export default function ImagePicker() {
+  const [image, setImage] = useState<string | null>(null);
+
+  const pickImage = async () => {
+    let result = await ExpoImagePicker.launchImageLibraryAsync({
+      mediaTypes: ExpoImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
+  return (
+    <View className="h-96 w-96">
+      <TouchableOpacity
+        className="w-full h-full flex items-center justify-center rounded-lg"
+        style={{ backgroundColor: '#EEF1F5' }}
+        onPress={pickImage}
+      >
+        <Image source={require('../assets/Frame.png')} />
+        <Text className="text-center"> Upload an Image </Text>
+      </TouchableOpacity>
+      {image && <Image source={{ uri: image }} />}
+    </View>
+  );
+}
