9b4933237f4a997ea23f69a06e279759ca0b55e4
ZainabImadulla <zainab.imadulla@icloud.com>
2024-10-13 01:37:20 -0400

    fixed format of components

--- frontend/components/bigtext.tsx
+++ frontend/components/bigtext.tsx
@@ -1,57 +1,55 @@
 import React, { forwardRef } from 'react';
 import { TextInput, View, Text } from 'react-native';
 
-
 interface TextInputProps {
-    title?: string;
-    placeholder?: string;
-    error?: string;
-    autoFocus?: boolean;
-    readOnly?: boolean;
-    inputMode?: 'text' | 'numeric';
-    onChangeText?: (value: string) => void;
-    value?: string;
-    maxLength?: number;
-    secureTextEntry?: boolean;
-  }
-  
-  const BigText = forwardRef<TextInput, TextInputProps>((props, ref) => {
-    const {
-      title,
-      placeholder,
-      error,
-      autoFocus,
-      readOnly,
-      inputMode = 'text',
-      onChangeText,
-      value,
-      maxLength,
-      secureTextEntry,
-    } = props;
-  
-    return (
-      <View className="w-full">
-        {title && <Text className="text-sm mb-1 font-bold">{title}</Text>}
-        <TextInput
-          ref={ref}
-          placeholder={placeholder}
-          placeholderTextColor="#a09b9b"
-          className={`h-[17vh] pt-[1.5vh] border rounded-[18px] px-[5%] bg-gray-200 ${
-            error ? 'border-red-500' : 'border-gray-300'
-          }`}
-          onChangeText={onChangeText}
-          value={value}
-          maxLength={maxLength}
-          secureTextEntry={secureTextEntry}
-          autoFocus={autoFocus}
-          editable={!readOnly}
-          multiline = {true}
-          keyboardType={inputMode === 'numeric' ? 'numeric' : 'default'}
-        />
-        {error && <Text className="text-red-500 text-xs mt-1">{error}</Text>}
-      </View>
-    );
-  });
-  
-  export default BigText;
-  
+  title?: string;
+  placeholder?: string;
+  error?: string;
+  autoFocus?: boolean;
+  readOnly?: boolean;
+  inputMode?: 'text' | 'numeric';
+  onChangeText?: (value: string) => void;
+  value?: string;
+  maxLength?: number;
+  secureTextEntry?: boolean;
+}
+
+const BigText = forwardRef<TextInput, TextInputProps>((props, ref) => {
+  const {
+    title,
+    placeholder,
+    error,
+    autoFocus,
+    readOnly,
+    inputMode = 'text',
+    onChangeText,
+    value,
+    maxLength,
+    secureTextEntry,
+  } = props;
+
+  return (
+    <View className="w-full">
+      {title && <Text className="text-sm mb-1 font-bold">{title}</Text>}
+      <TextInput
+        ref={ref}
+        placeholder={placeholder}
+        placeholderTextColor="#a09b9b"
+        className={`h-[17vh] pt-[1.5vh] border rounded-[18px] px-[5%] bg-gray-200 ${
+          error ? 'border-red-500' : 'border-gray-300'
+        }`}
+        onChangeText={onChangeText}
+        value={value}
+        maxLength={maxLength}
+        secureTextEntry={secureTextEntry}
+        autoFocus={autoFocus}
+        editable={!readOnly}
+        multiline={true}
+        keyboardType={inputMode === 'numeric' ? 'numeric' : 'default'}
+      />
+      {error && <Text className="text-red-500 text-xs mt-1">{error}</Text>}
+    </View>
+  );
+});
+
+export default BigText;
--- frontend/components/tag.tsx
+++ frontend/components/tag.tsx
@@ -1,18 +1,16 @@
-import React from "react"
-import { View, Text, Image} from "react-native"
+import React from 'react';
+import { View, Text, Image } from 'react-native';
 
-interface TagProps{
-    fish?: string
+interface TagProps {
+  fish?: string;
 }
-const Tag: React.FC<TagProps> = ({
-    fish
-}) => {
-    return (
-        <View className = "flex flex-row border rounded-[5px] border-[#7c8a9e] items-center justify-ceter py-1 px-2">
-            <Text className = 'font-medium text-[#7c8a9e]'> {fish} </Text>
-            <Image className = "h-2.5 w-2.5" source = {require('../assets/x.png')}/>
-        </View>
-    )
-}
+const Tag: React.FC<TagProps> = ({ fish }) => {
+  return (
+    <View className="flex flex-row border rounded-[5px] border-[#7c8a9e] items-center justify-ceter py-1 px-2">
+      <Text className="font-medium text-[#7c8a9e]"> {fish} </Text>
+      <Image className="h-2.5 w-2.5" source={require('../assets/x.png')} />
+    </View>
+  );
+};
 
-export default Tag
+export default Tag;
