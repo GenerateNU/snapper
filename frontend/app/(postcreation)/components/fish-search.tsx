1546e4a8b438a138189ddbb222d2ef1db8e2d6e4
ZainabImadulla <zainab.imadulla@icloud.com>
2024-10-16 21:49:50 -0400

    fixed bug with fish search

--- frontend/app/(postcreation)/components/fish-search.tsx
+++ frontend/app/(postcreation)/components/fish-search.tsx
@@ -3,51 +3,50 @@ import { TextInput, View, Image, Text, TouchableOpacity } from 'react-native';
 import { useFormContext } from 'react-hook-form';
 import { useState, useEffect } from 'react';
 
-type TagOption = {
-  name: string;
-  isEnabled: boolean;
-};
 
 export default function FishSearch() {
-  const initialFish: TagOption[] = [
-    { name: 'Anemone', isEnabled: true },
-    { name: 'Angelfish', isEnabled: false },
-    { name: 'Barnacle', isEnabled: false },
-    { name: 'Clown Fish', isEnabled: false },
-  ];
+  let data: string[] = ['Anemone', 'Angelfish', 'Barnacle', 'Clown Fish'];
+  const [visibility, setVisibility] = useState<boolean[]>([
+    false,
+    false,
+    false,
+    false,
+  ]);
 
-  const [possibleFish, setPossibleFish] = useState<TagOption[]>(initialFish);
-  const { setValue, watch } = useFormContext();
-  const tags = watch('tags', []);
+  const { setValue, watch} = useFormContext();
+  const tags = watch('tags') || [];
 
-  useEffect(() => {
-    // Create a new array with updated isEnabled values
-    const updatedFish = possibleFish.map((fish) => ({
-      ...fish, // Create a new object to avoid mutation
-      isEnabled: tags.includes(fish.name),
-    }));
-
-    //-------------------------------------------
-    //REMOVE THE COMMENT ON THE LINE BELOW FOR IT TO WORK ONCE THE APP HAS LOADED
-    //-------------------------------------------
-    // setPossibleFish(updatedFish);
-  }, [tags]);
-
-  const addRemoveTags = (index: number, shouldAdd: boolean) => {
-    if (shouldAdd) {
-      if (tags.includes(possibleFish[index].name)) {
-        // Return early if tag already exists
-        return;
-      }
-      console.log('Adding...');
-      setValue('tags', [possibleFish[index].name, ...tags]);
+  
+  const updateBoolAtIndex = (index: number, value: boolean) => {
+    const newVisibility = [...visibility];
+    newVisibility[index] = value;
+    if(value){
+        setValue('tags', [data[index], ...tags]);
     } else {
-      setValue(
-        'tags',
-        tags.filter((tag: string) => tag !== possibleFish[index].name)
-      );
+        setValue('tags', tags.filter((tag: string) => tag !== data[index]))
     }
+    setVisibility(newVisibility);
   };
+
+  useEffect(()=> {
+    const checkTags = () => {
+      let hasChanged = false;
+      const newVisibility = [...visibility];
+      for (let i: number = 0; i < 4; i++) {
+        if(!tags.includes(data[i]) && visibility[i]){
+          newVisibility[i] = false;
+          hasChanged = true
+        } else if(tags.includes(data[i]) && !visibility[i]){
+          newVisibility[i] = true;
+          hasChanged = true
+        }
+      }
+      if(hasChanged){
+        setVisibility(newVisibility);
+      }
+    }
+    checkTags()
+},[tags])
 
   return (
     <View className="h-[22vh] w-full">
@@ -61,21 +60,24 @@ export default function FishSearch() {
           className="pl-3 font-medium text-[14px]"
         />
       </View>
-      {possibleFish.map((item, index) => {
+      {data.map((item, index) => {
         return (
           <View
             key={index}
             className="flex flex-row border border-[#d2d9e2] basis-1/5 items-center pl-4"
           >
-            <Text className="text-[#7C8B9D] font-medium"> {item.name} </Text>
-            <TouchableOpacity
-              className={
-                item.isEnabled
-                  ? 'justify-self-end w-[2vh] h-[2vh] rounded-[3px] bg-[#d2d9e2] absolute right-5'
-                  : 'justify-self-end w-[2vh] h-[2vh] rounded-[3px] border border-[3px] border-[#d2d9e2] absolute right-5'
-              }
-              onPress={() => addRemoveTags(index, !item.isEnabled)}
-            />
+            <Text className="text-[#7C8B9D] font-medium"> {item} </Text>
+            {visibility[index] ? (
+              <TouchableOpacity
+                className="justify-self-end w-[2vh] h-[2vh] rounded-[3px] bg-[#d2d9e2] absolute right-5"
+                onPress={() => updateBoolAtIndex(index, false)}
+              />
+            ) : (
+              <TouchableOpacity
+                className="justify-self-end w-[2vh] h-[2vh] rounded-[3px] border border-[3px] border-[#d2d9e2] absolute right-5"
+                onPress={() => updateBoolAtIndex(index, true)}
+              />
+            )}
           </View>
         );
       })}
