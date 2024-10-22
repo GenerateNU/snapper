42ec965caf1c450476df00786aa8a7c9b78cdb05
Stone Liu <liu.sto@northeastern.edu>
2024-10-21 20:53:39 -0400

    Image picker use form context and provider

--- frontend/components/image-picker.tsx
+++ frontend/components/image-picker.tsx
@@ -1,9 +1,17 @@
 import { useState } from 'react';
 import { Image, TouchableOpacity, View, Text } from 'react-native';
 import * as ExpoImagePicker from 'expo-image-picker';
+import { useFormContext } from 'react-hook-form';
 
 export default function ImagePicker() {
+
   const [image, setImage] = useState<string | null>(null);
+
+  const context = useFormContext();
+
+  if (!context) throw Error("Please wrap the ImagePicker with a FormProvider from React Hook Form")
+
+  const setValue = context.setValue;
 
   const pickImage = async () => {
     let result = await ExpoImagePicker.launchImageLibraryAsync({
@@ -13,15 +21,18 @@ export default function ImagePicker() {
       quality: 1,
     });
 
-    console.log(result);
-
     if (!result.canceled) {
-      setImage(result.assets[0].uri);
+      const selectedImageUri = result.assets[0].uri;
+      setImage(selectedImageUri); // Update local state
+      setValue("image", selectedImageUri); // Set the form value to the selected image URI
+    } else {
+      setImage(null); // Clear the image state if canceled
+      setValue("image", undefined); // Clear the form value
     }
   };
 
   return (
-    <View className="h-96 w-96">
+    <View className="h-96 w-full">
       <TouchableOpacity
         className="w-full h-full flex items-center justify-center rounded-lg bg-gray-200"
         onPress={pickImage}
