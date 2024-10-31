b100a7b1a5ed5b2571b4d78e2b845ec692eab2d2
ZainabImadulla <zainab.imadulla@icloud.com>
2024-10-31 14:15:26 -0400

    finished tag fish page

--- frontend/app/(postcreation)/tag-fish.tsx
+++ frontend/app/(postcreation)/tag-fish.tsx
@@ -32,21 +32,24 @@ export default function TagFish(){
                     <Text className="font-normal text-center text-[14px] leading-[18px]"> Select the fish you saw on your dive</Text>
             </View>
 
-            <View className="flex flex-row border border-[#d2d9e2] rounded-md items-center pl-2 w-full h-[5vh] mb-5">
+            <View className="flex flex-row border border-[#d2d9e2] rounded-md items-center pl-2 w-full min-h-[5vh] mb-5">
                 <Image
                 className="h-[2.5vh] w-[2.5vh]"
                 source={require('../../assets/search.png')}
                 />
-                <View className = "flex h-full w-full flex-row items-center"> 
-                    <View className = "basis-1/5"> 
-                        <TextInput
-                            placeholder="Default "
-                            className="pl-3 font-medium text-[14px]"
-                        />
-                    </View>
-                    <View className = "w-full basis-3/5">
-                        <FishTags/>
-                    </View>
+                <View className = "flex h-full w-full flex-row items-center flex-wrap items-center gap-2 p-2"> 
+                    {tags.map((item, index) => {
+                    return (
+                        <View key = {index}> 
+                            <Tag fish={item} onPress = {() => removeFish(index)}/>
+                        </View>
+                    )
+                    })}
+                    <TextInput
+                        placeholder="Default "
+                        className="pl-3 font-medium text-[14px] h-[4vh]"
+                    />
+            
                     
                 </View>
             </View>
