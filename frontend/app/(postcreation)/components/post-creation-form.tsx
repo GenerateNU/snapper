17ded8963c0fca99f592a278b805ad75b4ea81e1
Josh T <torre.jo@northeastern.edu>
2024-11-03 14:22:21 -0500

    Image picker preview/style mostly working

--- frontend/app/(postcreation)/components/post-creation-form.tsx
+++ frontend/app/(postcreation)/components/post-creation-form.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {View, Text} from "react-native";
-import {useFormContext, Controller} from "react-hook-form"
+import { View, Text } from "react-native";
+import { useFormContext, Controller } from "react-hook-form"
 import ImagePicker from "../../../components/image-picker";
 import BigText from "../../../components/bigtext"
 import Input from '../../../components/input';
@@ -21,7 +21,7 @@ type FormFields = {
 
 export default function PostCreationForm() {
 
-    const {setValue, watch, reset} = useFormContext<FormFields>();
+    const { setValue, watch, reset } = useFormContext<FormFields>();
     const tags: string[] = watch('tags') || [];
 
     const removeFish = (index: number) => {
@@ -32,85 +32,90 @@ export default function PostCreationForm() {
         setValue('tags', newFish)
     };
 
-    const {control, trigger, register, handleSubmit} = useFormContext<FormFields>();
+    const { control, trigger, register, handleSubmit } = useFormContext<FormFields>();
 
     const submitPost = async (postData: FormFields) => {
-       try {
+        try {
             const response = await fetch(
-            'http://localhost:3000/divelog', {
-                method: 'POST', 
+                'http://localhost:3000/divelog', {
+                method: 'POST',
                 headers: {
-                'Content-Type': 'application/json'}, 
-                body: JSON.stringify(postData)})
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(postData)
+            })
         } catch (error) {
             console.error(error);
         }
         console.log(postData);
         reset();
     };
-        
-    
+
+
 
     return (
-        <View className = "space-y-2">
-            <View className = "h-[10vh] bg-gray-200 w-full">
+        <View className="space-y-2">
+            <View className="h-[10vh] bg-gray-200 w-full">
                 <Text> Insert image picker here</Text>
             </View>
             <Text className="text-[16px] pt-[2vh]">Caption</Text>
-            <View className = "mb-4">
+            <View className="mb-4">
                 <Controller
-                    control = {control}
-                    name = "description"
-                    render = {({field: {
-                        onChange, value}}) => (
-                    <BigText 
-                        placeholder="Your description..." 
-                        onChangeText={(text: string) => {
-                            onChange(text);
-                            trigger('description');}}
-                        value = {value}
-                    />
+                    control={control}
+                    name="description"
+                    render={({ field: {
+                        onChange, value } }) => (
+                        <BigText
+                            placeholder="Your description..."
+                            onChangeText={(text: string) => {
+                                onChange(text);
+                                trigger('description');
+                            }}
+                            value={value}
+                        />
                     )}
-                /> 
+                />
             </View>
             <Text className="text-[16px]"> Location </Text>
             <View>
                 <Controller
-                    control = {control}
-                    name = "location"
-                    render = {({field: {onChange, value}}) => (
-                    <Input 
-                        placeholder="Enter Location" 
-                        onChangeText={(text: string) => {
-                            onChange(text);
-                            trigger('location');}}
-                        value = {value}
-                    />
+                    control={control}
+                    name="location"
+                    render={({ field: { onChange, value } }) => (
+                        <Input
+                            placeholder="Enter Location"
+                            onChangeText={(text: string) => {
+                                onChange(text);
+                                trigger('location');
+                            }}
+                            value={value}
+                        />
                     )}
-                /> 
+                />
             </View>
-            <Text className = "text-[16px] ">Tag Fish</Text>
+            <Text className="text-[16px] ">Tag Fish</Text>
             <View className="w-full mb-[4vh]">
                 {tags.length == 0 ?
-                <PageButton outline = 'gray-300' text = "Choose Fish" backgroundColor= "white" onPress = {() => router.push('/tag-fish')} /> :
-                <View className="flex flex-row border border-[#d2d9e2] rounded-md items-center pl-2 w-full min-h-[5vh] mb-5">
-                    <View className = "flex h-full w-full flex-row items-center flex-wrap items-center gap-2 p-2"> 
-                        {tags.map((item, index) => {
-                        return (
-                            <View key = {index}> 
-                                <Tag fish={item} onPress = {() => removeFish(index)}/>
+                    <PageButton outline='gray-300' text="Choose Fish" backgroundColor="white" onPress={() => router.push('/tag-fish')} /> :
+                    <View className="flex flex-row border border-[#d2d9e2] rounded-md items-center pl-2 w-full min-h-[5vh] mb-5">
+                        <View className="flex h-full w-full flex-row items-center flex-wrap items-center gap-2 p-2">
+                            {tags.map((item, index) => {
+                                return (
+                                    <View key={index}>
+                                        <Tag fish={item} onPress={() => removeFish(index)} />
+                                    </View>
+                                )
+                            })}
+                            <View>
+                                <Button backgroundColor="white" color="ocean" text="+ Add More Fish" onPress={() => router.push('/tag-fish')}></Button>
                             </View>
-                        )
-                        })}
-                        <View>
-                            <Button backgroundColor = "white" color = "ocean" text= "+ Add More Fish" onPress = {() => router.push('/tag-fish')}></Button>     
                         </View>
                     </View>
-                 </View>
                 }
             </View>
-            <Button text = "Post" onPress = {handleSubmit(submitPost)} />
-            
+            <View className="pb-10">
+                <Button text="Post" onPress={handleSubmit(submitPost)} />
+            </View>
         </View>
     )
 }
