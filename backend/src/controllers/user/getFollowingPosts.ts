dea7dc5c1bb7da24dbb95eaef9087a21913a7d1b
mai <thisismainguyen@gmail.com>
2024-11-18 17:30:13 -0500

    renaming files

---
+++ backend/src/controllers/user/getFollowingPosts.ts
@@ -0,0 +1,40 @@
+import express from 'express';
+import { UserService, UserServiceImpl } from '../../services/userService';
+import { DEFAULT_LIMIT, DEFAULT_PAGE } from '../../consts/pagination';
+import { NotFoundError } from '../../consts/errors';
+import { ObjectId } from 'mongodb';
+
+const userService: UserService = new UserServiceImpl();
+
+export const getFollowingPosts = async (
+  req: express.Request,
+  res: express.Response,
+) => {
+  const userId = req.params.id;
+  const limit = parseInt(req.query.limit as string) || DEFAULT_LIMIT;
+  const page = parseInt(req.query.page as string) || DEFAULT_PAGE;
+
+  if (!userId) {
+    return res.status(400).json({ message: 'User ID is required' });
+  }
+
+  if (!ObjectId.isValid(userId)) {
+    return res.status(400).json({ error: 'Invalid user ID format' });
+  }
+
+  try {
+    const followingDiveLogs = await userService.getFollowingPosts(
+      userId,
+      limit,
+      page,
+    );
+    return res.status(200).json(followingDiveLogs);
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      return res.status(404).json({ error: error.message });
+    }
+    return res
+      .status(500)
+      .json({ error: 'An error occurred while retrieving divelogs' });
+  }
+};
---
+++ backend/src/controllers/user/toggleUserFollow.ts
@@ -0,0 +1,62 @@
+import express from 'express';
+import { UserService, UserServiceImpl } from '../../services/userService';
+import {
+  NotificationService,
+  NotificationServiceImpl,
+} from '../../services/notificationService';
+import { ExpoService, ExpoServiceImpl } from '../../services/expoService';
+import { NotFoundError } from '../../consts/errors';
+import { ObjectId } from 'mongodb';
+
+const notificationService: NotificationService = new NotificationServiceImpl();
+const userService: UserService = new UserServiceImpl();
+const expoService: ExpoService = new ExpoServiceImpl();
+
+export const toggleUserFollow = async (
+  req: express.Request,
+  res: express.Response,
+) => {
+  try {
+    const currentUserId = req.params.id;
+    const targetUserId = req.params.userId;
+
+    if (!currentUserId || !targetUserId) {
+      return res.status(400).json({ error: 'User ID is required' });
+    }
+
+    if (!ObjectId.isValid(currentUserId) || !ObjectId.isValid(targetUserId)) {
+      return res.status(400).json({ error: 'Invalid ID format' });
+    }
+
+    const followUser: any = await userService.toggleFollow(
+      currentUserId,
+      targetUserId,
+    );
+
+    const isFollowing = followUser.following.includes(targetUserId);
+
+    if (isFollowing) {
+      const notification = await notificationService.createFollowNotification(
+        currentUserId,
+        targetUserId,
+      );
+      if (notification) {
+        await expoService.sendFollowNotification(notification);
+      }
+      return res
+        .status(200)
+        .json({ message: 'Successfully followed the user.' });
+    } else {
+      return res
+        .status(200)
+        .json({ message: 'Successfully unfollowed the user.' });
+    }
+  } catch (error) {
+    if (error instanceof NotFoundError) {
+      return res.status(404).json({ error: error.message });
+    }
+    return res
+      .status(500)
+      .json({ error: 'An error occurred while toggling follow status.' });
+  }
+};
