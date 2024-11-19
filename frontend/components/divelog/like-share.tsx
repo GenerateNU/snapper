a8c94172f911201e1b225bc6a8c55a38a6b7cd06
ZainabImadulla <zainab.imadulla@icloud.com>
2024-11-18 20:51:15 -0500

    very very minor changes to fish tag page

--- frontend/components/divelog/like-share.tsx
+++ frontend/components/divelog/like-share.tsx
@@ -23,15 +23,6 @@ const LikeAndShare: React.FC<LikeAndShareProps> = ({ diveLogId }) => {
         url:
           link, 
       });
-      if (result.action === Share.sharedAction) {
-        if (result.activityType) {
-          // shared with activity type of result.activityType
-        } else {
-          // shared
-        }
-      } else if (result.action === Share.dismissedAction) {
-        // dismissed
-      }
     } catch (error: any) {
   
     }
